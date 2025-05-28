3e17e26fe2bc23fe50b29f8f2b95658e0d25e298
ferney1234567 <ariasfrancoferney2@gmail.com>
2025-05-28 15:05:33 -0500

    actualizo

--- api/v1/routes/articles.routes.js
+++ api/v1/routes/articles.routes.js
@@ -5,9 +5,9 @@ const userController = require('../../../controllers/articleController'); //se i
 const router = Router(); //se crea el router
 
 router.get("/", userController.getAllArticles); //se crea la ruta para obtener todos los articulos
-router.get("/:articleId", userController.getArticle);
+router.get("/:id", userController.getArticle);
 router.post("/", userController.createArticle); //se crea la ruta para crear un articulo
-router.put("/:articleId", userController.updateArticle); //se crea la ruta para actualizar un articulo
-router.delete("/:articleId", userController.deleteArticle);
+router.put("/:id", userController.updateArticle); //se crea la ruta para actualizar un articulo
+router.delete("/:id", userController.deleteArticle);
 
 module.exports = router;
