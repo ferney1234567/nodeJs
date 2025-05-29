57346049b23f04c40f1ed09b9f26389f65297041
ferney1234567 <ariasfrancoferney2@gmail.com>
2025-05-29 08:55:57 -0500

    enviar actualizacion

--- NodeJS/models/category.js
+++ NodeJS/models/category.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Category.belongsToMany(models.Article,{
-        through: 'articleCategories ',
+        through: 'articleCategories',
         as:"articles"
         });
       }
